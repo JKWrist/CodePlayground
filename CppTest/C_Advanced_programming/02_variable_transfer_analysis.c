645f714e954deb00a5a49681cc9cf2003c2e12fc
xujunze <xu_junze97@163.com>
2021-09-07 14:54:35 +0800

    要提交的变更： 	修改：     02_variable_transfer_analysis.c

--- CppTest/C_Advanced_programming/02_variable_transfer_analysis.c
+++ CppTest/C_Advanced_programming/02_variable_transfer_analysis.c
@@ -2,20 +2,55 @@
 #include <string.h>
 #include <stdlib.h>
 
-char * func()
+void func(char *p)
 {
-    char * p = malloc(10);
-    memset(p, 0, 10);
-    strcpy(p, "Hello");
-    return p;
+    strcpy(p, "hello worl");
 }
 
+void test01()
+{
+    //在test01中分配内存，分配在栈上
+    char buf[1024] = {0};
 
-//
+    func(buf);
 
+    printf("%s\n", buf);
+}
 
-//
+void printString(char *str)
+{
+    printf("%s\n", str + 6);
+}
 
+void test02()
+{
+    //分配内存到堆上
+    char *p = malloc(sizeof(char) * 64);
+    memset(p, 0, 64);
+    strcpy(p, "hello world");
+    printString(p);
+
+    if (p != NULL)
+    {
+        free(p);
+        p = NULL;
+    }
+}
+
+void alloacateSpace(char **p)
+{
+    *p = malloc(sizeof(char) * 64);
+    memset(*p, 0, 64);
+    strcpy(*p, "hello world");
+}
+
+void test03()
+{
+    char *p = NULL;
+    alloacateSpace(&p);
+
+    printf("%s\n", p);
+}
 
 /*
 * Author:xujunze
@@ -24,7 +59,9 @@ char * func()
 */
 int main()
 {
-   
-   return 0;
-}
+    test01();
 
+    test02();
+    test03();
+    return 0;
+}
