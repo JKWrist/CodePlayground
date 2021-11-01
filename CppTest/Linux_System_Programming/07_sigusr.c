aaa9f4e4c312b84fab5768e09875e923ca032824
xujunze <xu_junze97@163.com>
2021-11-01 13:38:04 +0800

    修改：     07_sigusr.c

--- CppTest/Linux_System_Programming/07_sigusr.c
+++ CppTest/Linux_System_Programming/07_sigusr.c
@@ -9,11 +9,11 @@
 #include <signal.h>
 
 int num = 0;
-int flag ;
+int flag;
 
 void func1(int signo)
 {
-    printf("F:[%d\n]\n", num);
+    printf("F:[%d]\n", num);
     num += 2;
     flag = 0;
     sleep(1);
@@ -21,9 +21,9 @@ void func1(int signo)
 
 void func2(int signo)
 {
-    printf("C:[%d\n]\n", num);
+    printf("C:[%d]\n", num);
     num += 2;
-    flag = 1;
+    flag = 0;
     sleep(1);
 }
 
@@ -41,41 +41,44 @@ int main(int argc, char *argv[])
     pid_t pid;
 
     pid = fork();
-    if(pid < 0)
+    if (pid < 0)
     {
         perror("fork error");
     }
-    else if(pid > 0)
+    else if (pid > 0)
     {
+        printf("F:[%d] C:[%d]\n", getpid(), pid);
+
         num = 0;
         flag = 1;
         signal(SIGUSR1, func1);
         while (1)
         {
-            if(flag == 0)
+            if (flag == 0)
             {
                 kill(pid, SIGUSR2);
                 flag = 1;
             }
+            sleep(1);
         }
-        
     }
-    else if(pid == 0)
+    else if (pid == 0)
     {
-        num = 1;
-        flag = 0;
-        signal(SIGUSR2, func2);
+        printf("C:[%d]\n", getpid());
 
+        num = 1;
+        flag = 0;   //先让子进程给父进程发信号
+        signal(SIGUSR2, func2);
         while (1)
         {
-            if(flag == 1)
+            if (flag == 0)   
             {
                 kill(getppid(), SIGUSR1);
-                flag = 0;
+                flag = 1;
             }
+            sleep(1);
         }
-        
     }
-    
+
     return 0;
 }
