ce6187447b731c3ab79195baa8d3c580af45d18c
xujunze <xu_junze97@163.com>
2021-11-13 13:04:51 +0800

    09_pthread_cond_mul.c 还是会有崩溃的问题存在

--- CppTest/Linux_System_Programming/09_pthread_cond_mul.c
+++ CppTest/Linux_System_Programming/09_pthread_cond_mul.c
@@ -24,21 +24,28 @@ pthread_cond_t cond;
 void *producer(void *arg)
 {
     printf("producer\n");
-    NODE *pNode;
+    NODE *pNode = NULL;
     int n = *(int *)arg;
     while (1)
     {
+        pNode = (NODE *)malloc(sizeof(NODE));
+        if(NULL == pNode)
+        {
+            perror("malloc error");
+            exit(-1);
+        }
         pNode->data = rand() % 1000;
-        pthread_mutex_lock(&mutex);
-
-        head->next = pNode;
-        head = pNode;
         printf("P [%d]:[%d]\n", n, pNode->data);
 
+        pthread_mutex_lock(&mutex);
+        head->next = head;
+        head = pNode;
         pthread_mutex_unlock(&mutex);
-        pthread_cond_signal(&cond);
 
-        usleep(rand() % 500000);
+
+        pthread_cond_signal(&cond);
+        //usleep(rand() % 500000);
+        sleep(rand() % 3);
     }
     pthread_exit(NULL);
 }
@@ -46,29 +53,38 @@ void *producer(void *arg)
 void *consumer(void *arg)
 {
     printf("consumer\n");
-    NODE *pNode;
+    NODE *pNode = NULL;
     int n = *(int *)arg;
     while (1)
     {
         pthread_mutex_lock(&mutex);
-
         if (NULL == head)
         {
+            //若条件不满足，需要阻塞等待
+            //若条件满足，解除阻塞并解锁
+            //若条件满足，生产者调用pthread_cond_signal函数通知，解除阻塞
             pthread_cond_wait(&cond, &mutex);
         }
 
-        pthread_mutex_lock(&mutex);
+        // if (NULL == head)
+        // {
+        //     pthread_mutex_unlock(&mutex);
+        //     continue;
+        // }
 
+        printf("C [%d]:[%d]\n", n, pNode->data);
         pNode = head;
         head = pNode->next;
-        printf("C [%d]:[%d]\n", n, pNode->data);
-        free(pNode);
-
         pthread_mutex_unlock(&mutex);
-        usleep(rand() % 500000);
+
+        free(pNode);
+        //usleep(rand() % 500000);
+        sleep(rand() % 3);
     }
     pthread_exit(NULL);
 }
+
+//gcc 09_pthread_cond_mul.c -lpthread
 
 /****************************************************************
  *  函数名称：多线程情况下的条件变量
@@ -90,13 +106,21 @@ int main(int argc, char *argv[])
     //条件变量初始化
     pthread_cond_init(&cond, NULL);
 
-    arr[5] = {0};
+    int arr[5] = {0};
     //创建生产者线程
     for (int i = 0; i < 5; i++)
     {
         arr[i] = i;
         ret = pthread_create(&thread1[i], NULL, producer, &arr[i]);
+        if(ret != 0)
+        {
+            printf("pthread_create %s\n", strerror(ret));
+        }
         ret = pthread_create(&thread2[i], NULL, consumer, &arr[i]);
+        if (ret != 0)
+        {
+            printf("pthread_create %s\n", strerror(ret));
+        }
     }
 
     //等待线程结束
