36c4d77dde0374e952ec0a8f005c9f3706607f47
xujunze <xu_junze97@163.com>
2021-11-16 21:54:12 +0800

    修改：     01_endian.c 新文件：   01_endian1.c

--- CppTest/Socket_Programing/01_endian.c
+++ CppTest/Socket_Programing/01_endian.c
@@ -32,9 +32,33 @@ void test01()
     // Net ordered address : 0x78563412
 }
 
+// 1、联合union是一个能在同一个存储空间存储不同类型数据的类型；
+// 2、同一内存段可以用来存放几种不同类型的成员，但每一瞬时只有一种起作用；
+
+union
+{
+    short s;
+    char c[sizeof(short)];
+} un2;
+
+union{
+    int s;
+    char c[sizeof(int)];
+}un4;
+
 void test02()
 {
+    printf("[%d][%d][%d]\n", sizeof(short), sizeof(int), sizeof(long int));
+    
+    //测试short类型
+    un2.s = 0x0102; //0x0102 =  16*16 + 2
+    printf("array after %p\n", &un2.c[1]);
+    printf("array before %p\n", &un2.c[0]);
+    printf("array after - before = %d\n", &un2.c[1] - &un2.c[0]);
 
+    //后面的地址是高地址，高地址存放高位数据是小端
+    printf("0x0102  01 是高位数据， 02是低位数据\n");
+    printf("低位地址数据：%d, 高位地址数据%d, %d\n", un2.c[0], un2.c[1], un2.s);
 }
 
 
@@ -50,7 +74,9 @@ void test02()
 ******************************************************************/
 int main(int argc, char *argv[])
 {
+    //test01();
 
+    test02();
 
     return 0;
 }
