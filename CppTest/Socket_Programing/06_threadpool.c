1ea0c4fbba176719072c3231df2c450e392c076e
xujunze <xu_junze97@163.com>
2021-12-26 22:04:24 +0800

    修改：     06_threadpool.c

--- CppTest/Socket_Programing/06_threadpool.c
+++ CppTest/Socket_Programing/06_threadpool.c
@@ -22,15 +22,47 @@ typedef struct
 //描述线程池的相关信息
 struct threadpool_t
 {
-    pthread_mutex_t lock;//用于锁住本结构体
+    pthread_mutex_t lock;           //用于锁住本结构体
     pthread_mutex_t thread_counter; //记录忙碌线程个数的锁 --busy_thr_num
     pthread_cond_t queue_not_full;  //任务队列满时，添加任务的线程阻塞，等待此条件变量
     pthread_cond_t queue_not_empty; //任务队列不为空时，通知等待任务的线程
-    
-    pthread_t * threads; //存放线程池中每个线程的tid，数组
-    pthread_t adjust_tid;
-    
+
+    pthread_t *threads;            //存放线程池中每个线程的tid，数组
+    pthread_t adjust_tid;          //管理线程的id
+    threadpool_task_t *task_queue; //任务队列、数组首地址
+
+    int min_thr_num;       //线程池最小线程数
+    int max_thr_num;       //线程池最大线程数
+    int live_thr_num;      //当前存货线程个数
+    int busy_thr_num;      //忙状态线程个数
+    int wait_exit_thr_num; //要销毁的线程个数
+
+    int queue_front;    //task_queue 队头下标
+    int queue_rear;     //task_queue 队尾下标
+    int queue_size;     //task_queue队中实际任务数
+    int queue_max_size; //task_queue队列可容纳任务数上限
+
+    int shutdown; //标志位，线程池使用状态 true or false
 };
+
+void * threadpool_thread(void * threadpool);
+
+void * adjust_thread(void * threadpool);
+
+void * is_thread_alive(threadpool_t * pool);
+
+//threadpool_create(3, 100, 100)
+threadpool_t * threadpool_create(int min_thr_num, int max_thr_num, int queue_max_size)
+{
+    int i;
+    threadpool_t * pool = NULL;
+
+    do
+    {
+        /* code */
+    } while (0);
+    
+}
 
 /****************************************************************
  *  函数名称：main
