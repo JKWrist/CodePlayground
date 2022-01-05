3639001928480a24802528f93f5b0f0eb593b22a
xujunze <xu_junze97@163.com>
2022-01-05 21:58:00 +0800

    新文件：   07_unix_client.c 新文件：   07_unix_server.c

---
+++ CppTest/Socket_Programing/07_unix_client.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <sys/un.h>
+#include <arpa/inet.h>
+
+/****************************************************************
+ *  函数名称：main
+ *  创建日期：2021-12-30 09:18:28
+ *  作者：xujunze
+ *  输入参数：无
+ *  输出参数：无
+ *  返回值：无
+******************************************************************/
+int main(int argc, char *argv[])
+{
+
+}
---
+++ CppTest/Socket_Programing/07_unix_server.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+
+/****************************************************************
+ *  函数名称：main
+ *  创建日期：2021-12-30 09:17:35
+ *  作者：xujunze
+ *  输入参数：无
+ *  输出参数：无
+ *  返回值：无
+******************************************************************/
+int main(int argc, char *argv[])
+{
+
+}
