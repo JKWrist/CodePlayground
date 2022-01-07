94c56994149ba42948c82c027f0c9e0aa916d507
xujunze <xu_junze97@163.com>
2022-01-07 21:07:46 +0800

    修改：     07_unix_client.c 修改：     07_unix_server.c

--- CppTest/Socket_Programing/07_unix_client.c
+++ CppTest/Socket_Programing/07_unix_client.c
@@ -20,5 +20,64 @@
 ******************************************************************/
 int main(int argc, char *argv[])
 {
+    //创建socket
+    int cfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if(cfd < 0)
+    {
+        perror("socket error");
+        return -1;
+    }
 
+    //删除socket文件，避免bind失败
+    //如果不删除 bind error: Address already in use
+    unlink("./client.sock");
+
+    //绑定bind
+    struct sockaddr_un client;
+    bzero(&client, sizeof(client));
+    client.sun_family = AF_UNIX;
+    strcpy(client.sun_path, "./client.sock");
+    int ret = bind(cfd, (struct sockaddr *)&client, sizeof(client));
+    if(ret < 0)
+    {
+        perror("bind error");
+        return -1;
+    }
+
+    struct sockaddr_un serv;
+    bzero(&serv, sizeof(serv));
+    serv.sun_family = AF_UNIX;
+    strcpy(serv.sun_path, "./server.sock");
+    ret = connect(cfd, (struct sockaddr *)&serv, sizeof(serv));
+
+    if(ret < 0)
+    {
+        perror("connect error");
+        return -1;
+    }
+    int n;
+    char buf[1024] = {0};
+
+    while (1)
+    {
+        memset(buf, 0, sizeof(buf));
+        n = read(STDIN_FILENO, buf, sizeof(buf));
+        printf("qqq\n");
+
+        //发送数据
+        write(cfd, buf, n);
+
+        printf("111\n");
+
+        //接收数据
+        memset(buf, 0, sizeof(buf));
+        n = read(cfd, buf, sizeof(buf));
+        if(n <= 0)
+        {
+            printf("read error or client close, n==[%d]\n", n);
+            break;
+        }
+        printf("read buf:[%s], n = %d\n", buf, n);
+    }
+    close(cfd);
 }
