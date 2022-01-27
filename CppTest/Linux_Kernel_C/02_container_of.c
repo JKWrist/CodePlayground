c7e1995e1afe9bb26c52dc61c66c656ad199b96b
xujunze <xu_junze97@163.com>
2022-01-27 18:32:52 +0800

    修改：     02_container_of.c

--- CppTest/Linux_Kernel_C/02_container_of.c
+++ CppTest/Linux_Kernel_C/02_container_of.c
@@ -18,12 +18,13 @@
 struct student
 {
 	int age;
-	char name[20];
+	char name[21];
 	char sex;
 };
 
 void test01()
 {
+	printf("---------------------------\n");
 	struct student stu = {18, "xiaohong", 'w'};
 	struct student *p;
 	p = container_of(&stu.name, struct student, name);
@@ -42,14 +43,41 @@ void test01()
 void test02()
 {
 	struct student stu = {18, "xiaohong", 'w'};
-	printf("%x\n", &stu);
-	printf("%x\n", &stu.age);
-	printf("%x\n", &stu.name);
-	printf("%x\n", &stu.sex);
+	printf("%p\n", &stu);
+	printf("%p\n", &stu.age);
+
+	char * a = &stu.name;
+	printf("%p\n", a);
+
+	char * b = &stu.sex;
+	printf("%p\n", b);
+
+	printf("%d\n", b - a);
+	printf("---------------------------\n");
+}
+
+/****************************************************************
+ *  函数名称：结构体成员的偏移
+ *  创建日期：2022-01-27 17:57:41
+ *  作者：xujunze
+ *  输入参数：无
+ *  输出参数：无
+ *  返回值：无
+******************************************************************/
+void test03()
+{
+	printf("&age = %p\n", &((struct student *)0)->age);
+	printf("&age = %d\n", &((struct student *)0)->age);
+	printf("&name = %p\n", &((struct student *)0)->name);
+	printf("&name = %d\n", &((struct student *)0)->name);
+	printf("&sex = %p\n", &((struct student *)0)->sex);
+	printf("&sex = %d\n", &((struct student *)0)->sex);
 	printf("---------------------------\n");
 }
 
 int main()
 {
 	test01();
+	test02();
+	test03();
 }
