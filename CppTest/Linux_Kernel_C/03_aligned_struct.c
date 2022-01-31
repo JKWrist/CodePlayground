5c7c5b38b6d37b746c9f1494841389a0e287296f
xujunze <xu_junze97@163.com>
2022-01-31 12:46:54 +0800

    新文件：   03_aligned.c 新文件：   03_aligned_struct.c 新文件：   03_section.c 新文件：   03_section1.c

---
+++ CppTest/Linux_Kernel_C/03_aligned_struct.c
@@ -0,0 +1,30 @@
+#pragma pack(1)
+
+typedef struct _STUDENT
+{
+	int a;   //0 ~ 3
+	char b;   //4
+	double c;  //5 ~ 13  double放到8的整数倍上
+	float d;	//16 ~ 20  float放到4的整数倍上
+} Student;
+
+typedef struct _STUDENT2
+{
+	char a;
+	Student b;
+	double c;
+} Student2;
+
+void test01()
+{
+	printf("sizeof Student:%d\n",sizeof(Student));
+	printf("sizeof Student2:%d\n",sizeof(Student2));
+	printf("---------------------------\n");
+}
+
+int main()
+{
+	test01();
+	
+	return 0;
+}
---
+++ CppTest/Linux_Kernel_C/03_section1.c
@@ -0,0 +1,8 @@
+int global_val = 8;
+int uninit_val_1;
+int uninit_val __attribute__ ((section(".data")));
+
+int main(void)
+{
+	return 0;
+}
