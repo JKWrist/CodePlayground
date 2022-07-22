fc8b5a2cda927e4a66b4987ebf923927edcaa6fc
xujunze <xu_junze97@163.com>
2022-07-22 21:19:18 +0800

    删除：     1.sh 删除：     2.sh 删除：     3.sh

--- ShellTest/2.sh
+++
@@ -1,8 +0,0 @@
-#£¡/bin/bash
-
-echo "PID for 2.sh : $$"
-echo "2.sh get \$A=$A from 1.sh"
-
-A=C
-export A
-echo "2.sh : \$A is $A"
--- ShellTest/3.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-file=/home/harrytc/Desktop/ShellTest/1.sh
-
-echo 111$(file#*/)
-echo 222$(file#*.)
